d0468d572cfbae09cc8a8cf6a49eb798b259599a
gimler <gimler@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-24 07:28:41 +0000

    sfGuardExtraPlugin fix validator

--- lib/form/base/BasesfGuardFormRegister.class.php
+++ lib/form/base/BasesfGuardFormRegister.class.php
@@ -15,7 +15,7 @@ class BasesfGuardFormRegister extends sfForm
       'username' => new sfValidatorDoctrineUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => 'username'), array('required' => 'Your username is required.', 'invalid' => 'This username already exists. Please choose another one.')),
       'email_address' => new sfValidatorEmail(array('trim' => true), array('required' => 'Your e-mail address is required.', 'invalid' => 'The email address is invalid.')),
       'password' => new sfValidatorString(array(), array('required' => 'Your password is required.')),
-      'password_confirmation' => new sfValidatorString(array(), array('Your password confirmation is required.')),
+      'password_confirmation' => new sfValidatorString(array(), array('required' => 'Your password confirmation is required.')),
     ));
 
     $this->validatorSchema->setPostValidator(new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_confirmation', array(), array('invalid' => 'The two passwords do not match')));
