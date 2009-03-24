22c7d93780202ae00c24d0bd837d12c72bc8b226
gimler <gimler@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-24 15:53:02 +0000

    sfGuardExtraPlugin: fix register validation

--- lib/form/base/BasesfGuardFormRegister.class.php
+++ lib/form/base/BasesfGuardFormRegister.class.php
@@ -12,21 +12,17 @@ class BasesfGuardFormRegister extends sfForm
     ));
 
     $this->setValidators(array(
-      'username' => new sfValidatorAnd(array(
-        new sfValidatorString(array('trim' => true), array('required' => 'Your username is required.')),
-        new sfValidatorPropelUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => 'username'), array('invalid' => 'This username already exists. Please choose another one.')),
-      # see ticket http://trac.symfony-project.org/ticket/4046
-      ), array(), array('required' => 'Your username is required.')),
-      'email_address' => new sfValidatorAnd(array(
-        new sfValidatorEmail(array('trim' => true), array('required' => 'Your e-mail address is required.', 'invalid' => 'The email address is invalid.')),
-        new sfValidatorPropelUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => 'email'), array('invalid' => 'This email already exists. Please choose another one.')),
-      # see ticket http://trac.symfony-project.org/ticket/4046
-      ), array(), array('required' => 'Your e-mail address is required.')),
+      'username' => new sfValidatorString(array('trim' => true), array('required' => 'Your username is required.')),
+      'email_address' => new sfValidatorEmail(array('trim' => true), array('required' => 'Your e-mail address is required.', 'invalid' => 'The email address is invalid.')),
       'password' => new sfValidatorString(array(), array('required' => 'Your password is required.')),
       'password_confirmation' => new sfValidatorString(array(), array('required' => 'Your password confirmation is required.')),
     ));
 
-    $this->validatorSchema->setPostValidator(new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_confirmation', array(), array('invalid' => 'The two passwords do not match')));
+    $this->validatorSchema->setPostValidator(new sfValidatorAnd(array(
+      new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_confirmation', array(), array('invalid' => 'The two passwords do not match'))
+      new sfValidatorPropelUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => array('username')), array('invalid' => 'This username already exists. Please choose another one.')),
+//      new sfValidatorPropelUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => array('email_adress')), array('invalid' => 'This email already exists. Please choose another one.')),
+    )));
 
     $this->widgetSchema->setNameFormat('register[%s]');
   }
