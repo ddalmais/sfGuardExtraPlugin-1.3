543e070eda8fafc2c6c4ab11150365092d4497d6
garak <garak@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-12-16 15:41:59 +0000

    [sfGuardExtraPlugin] enforce strong password with min length of 8

--- lib/form/plugin/PluginsfGuardFormRegister.class.php
+++ lib/form/plugin/PluginsfGuardFormRegister.class.php
@@ -19,14 +19,13 @@ class PluginsfGuardFormRegister extends BaseForm
     $this->setValidators(array(
       'username'         => new sfValidatorString(array('trim' => true), array('required' => 'Your username is required.')),
       'email'            => new sfValidatorEmail(array('trim' => true), array('required' => 'Your e-mail address is required.', 'invalid' => 'The email address is invalid.')),
-      'password'         => new sfValidatorString(array(), array('required' => 'Your password is required.')),
+      'password'         => new sfValidatorString(array('min_length' => 8), array('min_length' => 'Password is too short (%min_length% characters min).', 'required' => 'Your password is required.')),
       'password_confirm' => new sfValidatorString(array(), array('required' => 'Your password confirmation is required.')),
     ));
 
     $this->validatorSchema->setPostValidator(new sfValidatorAnd(array(
       new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_confirm', array(), array('invalid' => 'The two passwords do not match')),
       new sfValidatorPropelUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => array('username')), array('invalid' => 'This username already exists. Please choose another one.')),
-//    new sfValidatorPropelUnique(array('trim' => true, 'model' => 'sfGuardUser', 'column' => array('email_adress')), array('invalid' => 'This email already exists. Please choose another one.')),
     )));
 
     $this->widgetSchema->setNameFormat('register[%s]');
